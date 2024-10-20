a15ef561d093efdf144feecca7ea80fb2a3a2235
eder-ohtic <eder.villar@ohtic.com>
2024-10-20 15:56:52 +0300

    first commit

---
+++ Realtime-Messaging-Backend/src/main/java/es/evm/messenger/RealtimeMessagingBackendApplication.java
@@ -0,0 +1,13 @@
+package es.evm.messenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeMessagingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimeMessagingBackendApplication.class, args);
+	}
+
+}
---
+++ Realtime-Messaging-Backend/src/test/java/es/evm/messenger/RealtimeMessagingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package es.evm.messenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeMessagingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
